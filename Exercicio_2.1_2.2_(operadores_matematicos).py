d4e7b24eef6fa8f5c09e0ca3e83a3bd6fcf64d48
Josue <josueporto59@gmail.com>
2023-07-29 20:16:39 -0300

    operadores matematicos 2.1, 2.2

---
+++ Exercicio_2.1_2.2_(operadores_matematicos).py
@@ -0,0 +1,26 @@
+# Exercicio 2.1 
+
+'''
+Converta as seguintes expreções matemáticas para que possam ser calculadas usando o interpretador python.
+10 + 20 * 30
+4² / 30
+(9 ** 4 + 2) * 6 - 1 
+'''
+
+print((10 + 20) * 30)
+print()
+
+print((4 ** 2) / 30)
+print()
+
+print((9 ** 4 + 2) * (6) - 1)
+print()
+
+# Exercicio 2.2
+
+'''
+Digite a seguinte expreção no interpretador:
+10 / 3 *10 ** 2 + 1 - 10 * 4 / 2
+''' 
+
+print(10 / 3  * 10 ** 2 + 1 - 10 * 4 / 2)
