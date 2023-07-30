968087f1778f8aaf6eb9d8913772684fa0eddfc1
Josue <josueporto59@gmail.com>
2023-07-30 19:54:18 -0300

    Variaveis

---
+++ Programas,_2.1,_2.2,_2.3(Variaveis).py
@@ -0,0 +1,25 @@
+# Programa 2.1 - primeiro programa com variaveis:
+
+print('Programa 2.1\n')
+
+a = 2
+b = 3
+
+print(a + b)
+print('\n===================\n')
+
+# Programa 2.2 - cálculo de aumento de salário:
+
+print('Programa 2.2\n')
+
+salario = 1500
+aumento = 5
+
+print(salario + (salario * aumento / 100))
+print('\n===================\n')
+
+# Programa 2.3 - Alternativa para o cálculo de aumento de salário:
+
+print('Programa 2.3\n')
+
+print(1500 + (1500 * 5 / 100))
